7fc73b5fb813bab56692ce6e113c641ab2a8e040
NguyenDacCuong2604 <20130215@st.hcmuaf.edu.vn>
2024-10-14 09:11:55 +0700

    implementing the cartitem controller

--- src/main/java/com/example/keyboardshops/service/cart/ICartItemService.java
+++ src/main/java/com/example/keyboardshops/service/cart/ICartItemService.java
@@ -6,6 +6,5 @@ public interface ICartItemService {
     void addCartItem(Long cartId, Long productId, int quantity);
     void removeItemFromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
-
     CartItem getCartItem(Long cartId, Long productId);
 }
