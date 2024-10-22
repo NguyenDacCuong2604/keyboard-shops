7c98435fdf3b0b57afbd667198c72cd9a9239435
NguyenDacCuong2604 <20130215@st.hcmuaf.edu.vn>
2024-10-23 01:49:05 +0700

    implementing the user controllder

---
+++ src/main/java/com/example/keyboardshops/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.keyboardshops.controller;
+
+import com.example.keyboardshops.exceptions.AlreadyExistsException;
+import com.example.keyboardshops.exceptions.ResourceNotFoundException;
+import com.example.keyboardshops.model.User;
+import com.example.keyboardshops.request.CreateUserRequest;
+import com.example.keyboardshops.request.UserUpdateRequest;
+import com.example.keyboardshops.response.ApiResponse;
+import com.example.keyboardshops.service.user.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/users")
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/{userId}/user")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId){
+        try {
+            User user = userService.getUserById(userId);
+            return ResponseEntity.ok(new ApiResponse("Success", user));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createUser(@RequestParam CreateUserRequest request){
+        try {
+            User user = userService.createUser(request);
+            return ResponseEntity.ok(new ApiResponse("Create User Success!", user));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<ApiResponse> updateUser(@RequestParam UserUpdateRequest request, @PathVariable Long userId){
+        try {
+            User user = userService.updateUser(request, userId);
+            return ResponseEntity.ok(new ApiResponse("Update User Success!", user));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
+    @DeleteMapping("/{userId}/delete")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId){
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponse("Delete User Success!", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
